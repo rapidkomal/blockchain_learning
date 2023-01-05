9a42c39e8f8754625aea7d5f349ba1e7a434a857
rapidkomal <104576390+rapidkomal@users.noreply.github.com>
2023-01-05 14:16:13 +0530

    remove reentrancy dependencies from ownable

--- contracts/OwnableTodo.sol
+++ contracts/OwnableTodo.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract ToDoTask is ReentrancyGuard, Ownable {
+contract ToDoTask is Ownable {
     address private _owner;
 
     mapping(uint => Todo) public tasks;
